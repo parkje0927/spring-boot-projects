9800f7a570474e47dd293ada3c2cd4f86af17a9a
jh <parkje09270@gmail.com>
2021-10-24 17:18:21 +0900

    feat : login, logout config 추가

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,6 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Configuration //설정 클래스 이므로
 @EnableWebSecurity
@@ -17,6 +29,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         //인증
         http
-                .formLogin();
+                .formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId") //login 의 form 태그 속 username id
+                .passwordParameter("passwd") //login 의 form 태그 속 password id
+                .loginProcessingUrl("/login_proc") //login 의 form 태그 id
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication = " + authentication.getName());
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        System.out.println("exception = " + e.getMessage());
+                    }
+                })
+                .permitAll() //"/loginPage" 에 대한 permitAll
+
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login") //이동할 페이지
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                        HttpSession session = httpServletRequest.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                }) //다양한 로직 구현
+                .deleteCookies("remember-me");
     }
 }
